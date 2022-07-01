78d2605a2b90b41679edc4398189623840454fa6
Arijit Kundu <arikundu9@gmail.com>
2022-07-01 22:46:49 +0530

    jwt commented code added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// builder.Services.AddAuthentication(x =>
+//     {
+//         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//         x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//     }).AddJwtBearer(o =>
+//     {
+//         var Key = Encoding.UTF8.GetBytes(Configuration["JWT:Key"]);
+//         o.SaveToken = true;
+//         o.TokenValidationParameters = new TokenValidationParameters
+//         {
+//             ValidateIssuer = false,
+//             ValidateAudience = false,
+//             ValidateLifetime = true,
+//             ValidateIssuerSigningKey = true,
+//             ValidIssuer = Configuration["JWT:Issuer"],
+//             ValidAudience = Configuration["JWT:Audience"],
+//             IssuerSigningKey = new SymmetricSecurityKey(Key)
+//         };
+//     });
+// builder.Services.AddSingleton<IJWTManagerRepository, JWTManagerRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
