0d357cead77090f38a4ae0e3fdbc2f5ab38dae56
Arijit Kundu <arikundu9@gmail.com>
2022-07-02 01:11:06 +0530

    jwt working fine

---
+++ Models/Users.cs
@@ -0,0 +1,8 @@
+namespace JWTManager.Models
+{
+    public class Users
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using JWTWebAuthentication.Repository;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-// builder.Services.AddAuthentication(x =>
-//     {
-//         x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//         x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//     }).AddJwtBearer(o =>
-//     {
-//         var Key = Encoding.UTF8.GetBytes(Configuration["JWT:Key"]);
-//         o.SaveToken = true;
-//         o.TokenValidationParameters = new TokenValidationParameters
-//         {
-//             ValidateIssuer = false,
-//             ValidateAudience = false,
-//             ValidateLifetime = true,
-//             ValidateIssuerSigningKey = true,
-//             ValidIssuer = Configuration["JWT:Issuer"],
-//             ValidAudience = Configuration["JWT:Audience"],
-//             IssuerSigningKey = new SymmetricSecurityKey(Key)
-//         };
-//     });
-// builder.Services.AddSingleton<IJWTManagerRepository, JWTManagerRepository>();
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(o =>
+    {
+        var Key = Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]);
+        o.SaveToken = true;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Key)
+        };
+    });
+builder.Services.AddSingleton<IJWTManagerRepository, JWTManagerRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -36,6 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
